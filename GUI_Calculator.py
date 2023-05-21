92e142fa55aa75ff2f8fc7212b9285d28aeb71c7
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-05-21 09:12:38 +0800

    Were currently trying to make a screen like interface to the calculator. We encountered an error must fix

--- GUI_Calculator.py
+++ GUI_Calculator.py
@@ -13,6 +13,9 @@ root.resizable(0,0)
 root.configure(background='black')
 root.mainloop()
 #create a space where the calculation takes place or the screen of a calculator
+Num_Input = Label(root, text='', bg='black', fg='white')
+Num_Input.grid(row=0, column=0, pady=(50,25), columnspan=5, sticky='w')
+Num_Input.config(font=('verdana',30, 'bold'))
 #create a functioning button for 7
 #create a functioning button for 8
 #create a functioning button for 9
